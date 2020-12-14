eebaf8e67a837ab00dc0c2718dd7e931ed79620d
2439905184 <suchenglong018@126.com>
2020-12-14 19:52:02 +0800

    添加了header参数() 常用代码模板

--- php模板.php
+++ php模板.php
@@ -1,3 +1,11 @@
 <?php
+//html http头
 header("Content-Type:text/html;charset=utf-8");
+//纯文本http头
+header("Content-Type:text/plain;charset=utf-8");
+//php允许跨域访问
+header('Access-Control-Allow-Origin:*'); // *代表允许任何网址请求
+header('Access-Control-Allow-Methods:POST,GET'); // 允许请求的类型
+/*全部参数*/
+//header('Access-Control-Allow-Methods:POST,GET,OPTIONS,DELETE'); // 允许请求的类型
 ?>
