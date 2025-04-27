37b4bb8ef7fabc5f395a0451c2360bc86d041323
2439905184 <2439905184@qq.com>
2025-04-27 10:35:37 +0800

    补充js工具

---
+++ js/jsontoli.js
@@ -0,0 +1,13 @@
+// root参数是ol或者ul元素
+function jsonToLi(json,root) 
+{
+    for(var key in json)
+    {
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        a.innerText = key
+        a.href = json[key]
+        li.appendChild(a);
+        root.appendChild(li);
+    }    
+}
